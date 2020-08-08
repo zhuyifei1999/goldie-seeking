c38d4663892cd73a78a51079529fe7c90eb14764
YiFei Zhu <zhuyifei1999@gmail.com>
2020-08-08 09:10:54 -0500

    Fix sorting

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-for map in gusher\ graphs/*.txt; do python stratfmt.py "${map}" map.bin && ./strats map.bin | sort -k3,4 -n > out/"$(basename "${map}")"; done
+for map in gusher\ graphs/*.txt; do python stratfmt.py "${map}" map.bin && ./strats map.bin | sort -k2,2n -k3,3g -s > out/"$(basename "${map}")"; done
